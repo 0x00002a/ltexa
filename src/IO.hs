becc11fa9f314b63141095d6cda6b1b035a8ac0a
0x00002a <ashenglandelbro@protonmail.com>
2020-11-23 00:28:35 +0000

    Added missing IO file

---
+++ src/IO.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE CPP #-}
+
+module IO where
+
+import qualified Data.ByteString as B
+import Data.Text (Text, pack, unpack)
+import PrettyPrint (PrettyPrintable (..), rstrip)
+import System.IO
+  ( Handle (..),
+    IOMode (..),
+    stderr,
+    stdout,
+    withFile,
+  )
+import Text.Parsec
+  ( SourcePos,
+    sourceColumn,
+    sourceLine,
+  )
+import Text.PrettyPrint.ANSI.Leijen
+  ( (<+>),
+    (<>),
+  )
+import qualified Text.PrettyPrint.ANSI.Leijen as C
+import Types
+
+readAll :: InFileType -> IO B.ByteString
+readAll StdinFT = B.getContents
+readAll (PathST fp) = B.readFile fp
+
+printVersion :: IO ()
+printVersion =
+  pInfo
+    >> pVer
+    >> putStrLn ""
+  where
+    pVer = putStr $ show VERSION_ltexa
+    pInfo = putStr "LTeXa "
+
+instance PrettyPrintable ParseMessage where
+  formatDoc (Msg (ParseMessageData title body line tp page fp)) =
+    path
+      <> printLine
+      <> formatDoc tp
+      <> C.text (unpack $ rstrip body)
+      <> printPage
+    where
+      path = case fp of
+        Just p -> C.bold $ C.text $ p ++ ":"
+        Nothing -> C.text ""
+
+      printLine = case line of
+        Just ln ->
+          C.bold (C.cyan $ C.text $ show ln) <> C.text ": "
+        Nothing -> C.text " "
+      printPage = C.text $ " (on page: " ++ show page ++ ")"
+  formatDoc (AppMsg (AppMessage what pos tp)) =
+    prefix
+      <+> pType
+      <+> msg
+      <+> location
+    where
+      prefix = C.bold $ C.magenta $ C.text "[LTeXa]:"
+      pType = formatDoc tp
+      location = formatDoc pos
+      msg = C.text $ unpack what
+
+instance PrettyPrintable SourcePos where
+  formatDoc pos =
+    C.text $
+      "(line: " ++ (show (sourceLine pos))
+        ++ ", col: "
+        ++ (show (sourceColumn pos))
+        ++ ")"
+
+instance PrettyPrintable MessageType where
+  formatDoc msg = chooseColour $ C.text $ show msg ++ ": "
+    where
+      chooseColour = case msg of
+        ErrMsg -> C.red
+        WarnMsg -> C.yellow
+        InfoMsg -> C.blue
+        TraceMsg -> C.dullwhite
