da6c3f6bddb5783072a60b27ef92680ce338ee0f
0x00002a <ashenglandelbro@protonmail.com>
2021-01-20 20:41:55 +0000

    Added missing file

---
+++ src/Constants.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE CPP #-}
+
+module Constants where
+
+version :: String
+version = VERSION_ltexa
+
+progName :: String
+progName = "LTeXa"
+
+versionStr :: String
+versionStr = progName ++ " v" ++ version
